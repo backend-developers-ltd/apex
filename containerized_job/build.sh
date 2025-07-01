c3784fbba51ba0a9883ec327158207b807c2e09b
chrisu-inigra <krzysztof.urbanowski@inigra.eu>
2025-07-01 13:55:38 +0200

    Add BLOCK_STARTUP ENV

--- containerized_job/build.sh
+++ containerized_job/build.sh
@@ -6,12 +6,14 @@ VLLM_GPU_UTILIZATION=0.8
 
 IMAGE_NAME="backenddevelopersltd/ch-sn1-job"
 HF_MODEL_PATH="./hf_model"
+BLOCK_STARTUP="false"
 
 DOCKER_BUILDKIT=1 docker build \
     --build-arg MODEL_ID="$MODEL_ID" \
     --build-arg VLLM_MODEL_ID="$VLLM_MODEL_ID" \
-    --build-arg HF_MODEL_PATH="$HF_MODEL_PATH" \
     --build-arg VLLM_GPU_UTILIZATION="$VLLM_GPU_UTILIZATION" \
+    --build-arg HF_MODEL_PATH="$HF_MODEL_PATH" \
+    --build-arg BLOCK_STARTUP="BLOCK_STARTUP" \
     -t "$IMAGE_NAME" \
     --build-context project_context=../ \
     .
